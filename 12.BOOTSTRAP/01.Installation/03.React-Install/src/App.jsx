413ff17ca24a8b206e7c6eb838336c52aff0ec53
MAYANKSHARMA01010 <sharmamayank01010@gmail.com>
2024-12-26 17:55:25 +0530

    Bootstrap Done Till Color

---
+++ 12.BOOTSTRAP/01.Installation/03.React-Install/src/App.jsx
@@ -0,0 +1,14 @@
+// installing React Bootstrap CSS 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-danger">Welcome to Bootstrap</h1>
+    </>
+  )
+}
+
+export default App
